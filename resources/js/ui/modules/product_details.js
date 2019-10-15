5d1545f08a62c65d178424c8edd9386bb521c553
luiz-gustavo <31047332+luiz-gustavo@users.noreply.github.com>
2019-10-15 16:10:41 -0300

    Update product_details.js

--- resources/js/ui/modules/product_details.js
+++ resources/js/ui/modules/product_details.js
@@ -16,42 +16,6 @@ $(document).ready(function () {
         }
     });
 
-    let $easyzoom = $(".easyzoom");
-    if (!isMobile()) {
-        $easyzoom.easyZoom().init();
-    }
-    // Setup thumbnails example
-
-    $('.thumbnails').on('click', 'a', function(e) {
-        let $this = $(this);
-        let $toggle = $('.toggleZoom');
-
-        e.preventDefault();
-
-        $this.parents(".slick-slide").siblings().removeClass("slick-current")
-        $this.parents(".slick-slide").addClass("slick-current")
-
-        if ($toggle.data("active") === true || !isMobile()) {
-            $easyzoom.easyZoom().filter('.easyzoom--with-thumbnails').data('easyZoom').swap($this.data('standard'), $this.attr('href'));
-        } else {
-            $(">a", ".easyzoom").attr("href", $this.data('standard'))
-            $(">a>img", ".easyzoom").attr("src", $this.data('standard'))
-        }
-
-
-    });
-
-    $('.toggleZoom').on('click', function() {
-        var $this = $(this);
-
-        if ($this.data("active") === true) {
-            $this.text("Habilitar Zoom").data("active", false);
-            $easyzoom.easyZoom().filter('.easyzoom--with-toggle').data('easyZoom').teardown();
-        } else {
-            $this.text("Desativar Zoom").data("active", true);
-            $easyzoom.easyZoom().init();
-        }
-    });
     
     /*
     Funcao para ativar a aba avaliacao
